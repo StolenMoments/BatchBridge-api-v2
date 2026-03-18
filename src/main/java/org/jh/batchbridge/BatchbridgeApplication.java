cf848a1247b5020f6a4b6391216e84c6a8caf490
Jonghwa Lee <unknown9732@naver.com>
2026-03-18 18:42:27 +0900

    init

---
+++ src/main/java/org/jh/batchbridge/BatchbridgeApplication.java
@@ -0,0 +1,13 @@
+package org.jh.batchbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchbridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchbridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jh/batchbridge/BatchbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jh.batchbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchbridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
