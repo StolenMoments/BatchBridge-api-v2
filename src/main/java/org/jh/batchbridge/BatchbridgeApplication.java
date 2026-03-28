2ed20cf49e73588592e5d753e6485d3b828c0261
Jonghwa Lee <unknown9732@naver.com>
2026-03-28 14:41:58 +0900

    [DEV-58] Fix: Enable JPA Auditing for createdAt field

--- src/main/java/org/jh/batchbridge/BatchbridgeApplication.java
+++ src/main/java/org/jh/batchbridge/BatchbridgeApplication.java
@@ -3,11 +3,13 @@ package org.jh.batchbridge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
+@EnableJpaAuditing
 public class BatchbridgeApplication {
 
     public static void main(String[] args) {
