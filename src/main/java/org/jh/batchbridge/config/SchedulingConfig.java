d42d398d3cee004eb111f43c8349d5aed3d8717e
Jonghwa Lee <unknown9732@naver.com>
2026-04-04 12:49:22 +0900

    [Fix] Separate infrastructure configuration from Application class
    - Move @EnableJpaAuditing and @EnableScheduling to separate configuration classes.
    - This prevents @WebMvcTest from failing due to missing JPA metamodel.
    - Grant execute permission to gradlew.

---
+++ src/main/java/org/jh/batchbridge/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package org.jh.batchbridge.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
